9d6717afffb34e3b4498c7c9139060746e3ce598
nathanaeltchamba <natanshost@gmail.com>
2023-05-29 11:22:55 -0500

    first commit

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const metadata = {
+    title: 'About Nate\'s Github',
+}
+
+const AboutPage = () => {
+  return (
+    <div>AboutPage</div>
+  )
+}
+
+export default AboutPage
